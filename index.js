c7ef9d58b9f7586857588deba28b687ee391e9dc
Airiel Altemara <vsokord#gmail.com>
2022-10-13 14:02:30 -0700

    completed topic 4

--- index.js
+++ index.js
@@ -201,8 +201,10 @@ function greeting(string1, string2){
 - Instances of CuboidMaker should initialize `length`, `width` and `height` properties
 */
 
-function CuboidMaker(/*Your Code Here */){
-  /*Your Code Here */
+function CuboidMaker(props){
+  this.length = props.length
+  this.width = props.width
+  this.height = props.height
 }
 
 
@@ -210,7 +212,10 @@ function CuboidMaker(/*Your Code Here */){
   Create a method called volume using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   💡 NOTE: Formula for cuboid volume: length * width * height   
 */
-
+CuboidMaker.prototype.volume = function(props) {
+  let vol = this.length * this.width * this.height
+  return vol
+}
 
 
 
@@ -218,7 +223,10 @@ function CuboidMaker(/*Your Code Here */){
   Create another method called surfaceArea using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
   💡 NOTE: Formula for cuboid surface area: 2 * (length * width + length * height + width * height)  
 */
-
+CuboidMaker.prototype.surfaceArea = function(props) {
+  let surfAre = 2 *((this.length * this.width) + (this.length * this.height) + (this.width * this.height))
+  return surfAre
+}
 
 
 
@@ -226,12 +234,18 @@ function CuboidMaker(/*Your Code Here */){
   Create an object called cuboid that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid. */
 
-
+const cuboid = new CuboidMaker({
+  length: 4, 
+  width: 5, 
+  height: 5
+})
 
 
 
 // 🐴🐴🐴 Test your volume and surfaceArea methods by uncommenting the logs below: 🐴🐴🐴
 // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
+
+// console.log(cuboid); // 100
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
  
@@ -240,7 +254,19 @@ function CuboidMaker(/*Your Code Here */){
 //Using CuboidMakerTwo, take your prototypes from above and refactor into class syntax. Then, create an object called cuboidTwo that uses the new keyword to use our CuboidMakerTwo class.
  
 class CuboidMakerTwo{
-
+  constructor(props){
+  this.length = props.length
+  this.width = props.width
+  this.height = props.height
+  }
+  volume(props) {
+    let vol = this.length * this.width * this.height
+    return vol
+  }
+  surfaceArea(props) {
+    let surfAre = 2 *((this.length * this.width) + (this.length * this.height) + (this.width * this.height))
+    return surfAre
+  }
 }
 
 
